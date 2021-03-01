a3b0e97329101e871a9c6273db3c0de2b1372366
sealice <15445553+sealice@users.noreply.github.com>
2021-03-01 21:47:07 +0800

    Optimize request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,120 +1,17 @@
 import axios from 'axios';
 import buildURL from 'axios/lib/helpers/buildURL';
-import { merge, deepMerge, isString, isUndefined } from 'axios/lib/utils';
+import { merge, isString, isUndefined } from 'axios/lib/utils';
 import { Message } from 'element-ui';
 import bus from './bus';
 
-function isPlainObject(val) {
-    return val && val.constructor.name === 'Object';
-}
-
-let acitveAxios = 0;
-let loadingFlag;
-let timer;
-
-const setLoading = loadingText => {
-    if (!loadingFlag && acitveAxios > 0) {
-        loadingFlag = true;
-        bus.emit('loading:show', { text: isString(loadingText) ? loadingText : '' });
-
-        // 优化loading的显示，保证用250ms的时间显示loading
-        // 避免请求响应在400-600ms之间时，loading闪现问题（loading还未来得及显示就关闭了）
-        if (acitveAxios == 1) {
-            acitveAxios++;
-            setTimeout(closeLoading, 350);
-        }
-    }
+// 默认配置
+const defaults = {
+    // baseURL: '',
+    // timeout: 15000, // 默认请求超时/ms
+    // emulateJSON: true, // 是否默认表单提交
 };
 
-const showLoading = loading => {
-    acitveAxios++;
-
-    if (!isUndefined(loading) && !loading) {
-        return;
-    }
-
-    timer = timer && clearTimeout(timer);
-    if (loading) {
-        setLoading(loading);
-    } else {
-        // 400ms内请求还有没有响应则显示loading
-        timer = setTimeout(setLoading, 400);
-    }
-};
-
-const closeLoading = () => {
-    acitveAxios--;
-    if (acitveAxios <= 0) {
-        timer = timer && clearTimeout(timer);
-        loadingFlag = false;
-        bus.emit('loading:hide');
-    }
-};
-
-// 请求拦截
-const requestInterceptors = [
-    cfg => {
-        showLoading(cfg.loading); // 传入真值立即显示loading，如果为字符串还会显示加载文本
-
-        // 表单提交
-        if (cfg.emulateJSON && isPlainObject(cfg.data)) {
-            cfg.data = buildURL('', cfg.data).substr(1);
-        }
-
-        // 配置开发模式的代理环境
-        if (process.env.NODE_ENV === 'development') {
-            const proxyEnv = localStorage.proxyEnv || 'dev';
-            const proxyHost = require('../../mock.proxy');
-            if (proxyHost[proxyEnv]) {
-                cfg.headers = cfg.headers || {};
-                cfg.headers['X-Mock-Proxy'] = proxyHost[proxyEnv];
-            }
-        }
-
-        return cfg;
-    },
-];
-
-// 响应拦截
-const responseInterceptors = [
-    res => {
-        closeLoading();
-
-        const { config: cfg, data } = res;
-        res.ok = true;
-
-        if (!cfg.noIntercept) {
-            if (data.code == 0) {
-                return data;
-            }
-
-            if (cfg.errMsg || isUndefined(cfg.errMsg)) {
-                Message.error(cfg.errMsg || data.msg || '系统繁忙！');
-            }
-
-            return Promise.reject(res);
-        }
-
-        return res;
-    },
-    err => {
-        closeLoading();
-
-        const { request: req } = err;
-        err.ok = false;
-
-        switch (req.status) {
-            // case 401:
-            //     // do something
-            //     break;
-            default:
-                Message.error(statusText[req.status] || req.statusText || err.message);
-        }
-
-        return Promise.reject(err);
-    },
-];
-
+// 响应状态
 const statusText = {
     '0': '网络错误！',
     // '400': '错误的请求！',
@@ -131,21 +28,124 @@ const statusText = {
     // '505': 'HTTP协议版本不受支持！',
 };
 
-const defaults = {
-    // baseURL: '',
-    // timeout: 15000, // 默认请求超时/ms
-    // emulateJSON: true, // 是否默认表单提交
-};
+let acitveAxios = 0;
+let loadingFlag;
+let timer;
 
-const serviceCreate = config => {
+function closeLoading() {
+    acitveAxios--;
+    if (acitveAxios <= 0) {
+        timer = timer && clearTimeout(timer);
+        loadingFlag = false;
+        bus.emit('loading:hide');
+    }
+}
+
+function setLoading(loadingText) {
+    if (!loadingFlag && acitveAxios > 0) {
+        loadingFlag = true;
+        bus.emit('loading:show', { text: isString(loadingText) ? loadingText : '' });
+
+        // 优化loading的显示，保证用250ms的时间显示loading
+        // 避免请求响应在400-600ms之间时，loading闪现问题（loading还未来得及显示就关闭了）
+        if (acitveAxios == 1) {
+            acitveAxios++;
+            setTimeout(closeLoading, 350);
+        }
+    }
+}
+
+function showLoading(loading) {
+    acitveAxios++;
+
+    if (!isUndefined(loading) && !loading) {
+        return;
+    }
+
+    timer = timer && clearTimeout(timer);
+    if (loading) {
+        setLoading(loading);
+    } else {
+        // 400ms内请求还有没有响应则显示loading
+        timer = setTimeout(setLoading, 400);
+    }
+}
+
+function isPlainObject(val) {
+    return val && val.constructor.name === 'Object';
+}
+
+// 请求拦截
+function onFulfilledRequest(cfg) {
+    showLoading(cfg.loading); // 传入真值立即显示loading，如果为字符串还会显示加载文本
+
+    // 表单提交
+    if (cfg.emulateJSON && isPlainObject(cfg.data)) {
+        cfg.data = buildURL('', cfg.data).substr(1);
+    }
+
+    // 配置开发模式的代理环境
+    if (process.env.NODE_ENV === 'development') {
+        const proxyEnv = localStorage.proxyEnv || 'dev';
+        const proxyHost = require('../../mock.proxy');
+        if (proxyHost[proxyEnv]) {
+            cfg.headers = cfg.headers || {};
+            cfg.headers['X-Mock-Proxy'] = proxyHost[proxyEnv];
+        }
+    }
+
+    return cfg;
+}
+
+// 响应拦截(成功)
+function onFulfilledResponse(res) {
+    closeLoading();
+
+    const { config: cfg, data } = res;
+    res.ok = true;
+
+    if (!cfg.noIntercept) {
+        if (data.code == 0) {
+            return data;
+        }
+
+        if (cfg.errMsg || isUndefined(cfg.errMsg)) {
+            Message.error(cfg.errMsg || data.msg || '系统繁忙！');
+        }
+
+        return Promise.reject(res);
+    }
+
+    return res;
+}
+
+// 响应拦截(失败)
+function onRejectedResponse(err) {
+    closeLoading();
+
+    const { request: req } = err;
+    err.ok = false;
+
+    switch (req.status) {
+        // case 401:
+        //     // do something
+        //     break;
+        default:
+            ElMessage.error(statusText[req.status] || req.statusText || err.message);
+    }
+
+    return Promise.reject(err);
+}
+
+export function serviceCreate(config) {
     const service = axios.create(merge(defaults, config));
-    service.interceptors.request.use(...requestInterceptors);
-    service.interceptors.response.use(...responseInterceptors);
+    service.interceptors.request.use(onFulfilledRequest);
+    service.interceptors.response.use(onFulfilledResponse, onRejectedResponse);
 
     return service;
-};
+}
 
-const service = serviceCreate();
+export const service = serviceCreate();
 
 /** 默认不带参数的Get请求 */
 export function get(url) {
@@ -161,7 +161,5 @@ export function getParams(url) {
 export function post(url) {
     return (data, config) => service.post(url, data, config);
 }
-
-export { serviceCreate, service, merge, deepMerge };
 
 export default service;
