163bef361615c3eb53d2ceafb5d2a44ab75e4b3f
sealice <15445553+sealice@users.noreply.github.com>
2021-05-15 15:31:56 +0800

    finxed

--- src/utils/bus.js
+++ src/utils/bus.js
@@ -3,9 +3,9 @@ import mitt from 'mitt';
 const bus = mitt();
 
 bus.once = (type, handler) => {
-    const wrappedHandler = evt => {
-        handler(evt);
+    const wrappedHandler = (eventOrType, event) => {
         bus.off(type, wrappedHandler);
+        handler(eventOrType, event);
     };
     bus.on(type, wrappedHandler);
     return wrappedHandler;
